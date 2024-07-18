6ff5e41a18b4092bae06d57113eb980c7333ace0
Lawson Allick <lallick@gmail.com>
2024-07-18 13:01:47 -0400

    Add functionality and styling to login and register page

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,3 +1,48 @@
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+  async function login(e) {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3000/api/v1/auth/login", {
+        method: "POST",
+        body: JSON.stringify({ username, password }),
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await response.json();
+      if (response.ok) {
+        localStorage.setItem("jwt", data.token);
+        setRedirect(true);
+      } else {
+        alert(data.message);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
+  return (
+    <form className="login" onSubmit={login}>
+      <h1>Login</h1>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button>Login</button>
+    </form>
+  );
 }
